fce3b390d6fe4193d205dbdb123dc9800d5c321d
MrAlPatino <43314815+MrAlPatino@users.noreply.github.com>
2021-05-26 18:54:05 +0100

    Add files via upload

---
+++ src/main/java/net/romvoid95/TestPod.java
@@ -0,0 +1,10 @@
+package net.romvoid95;
+import net.minecraftforge.common.*;
+import net.minecraft.*;
+import net.romvoid95.*;
+
+
+public class TestPod {
+    //This is an test code for the new system of packed bases (pods for short) this code is meant to make them spawn them via /pod
+
+}
